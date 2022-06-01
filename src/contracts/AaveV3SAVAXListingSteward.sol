edcc67727a5f75a69ecfebb188936910d3d44242
eboado <ebdmrr@gmail.com>
2022-06-01 13:54:34 +0200

    - Added final sAVAX listing steward with listing tests.

---
+++ src/contracts/AaveV3SAVAXListingSteward.sol
@@ -0,0 +1,143 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {IPoolConfigurator, ConfiguratorInputTypes} from './interfaces/IPoolConfigurator.sol';
+import {IACLManager} from './interfaces/IACLManager.sol';
+import {IAaveOracle} from './interfaces/IAaveOracle.sol';
+import {Ownable} from './dependencies/Ownable.sol';
+
+contract AaveV3SAVAXListingSteward is Ownable {
+    // **************************
+    // Protocol's contracts
+    // **************************
+
+    IPoolConfigurator public constant CONFIGURATOR =
+        IPoolConfigurator(0x8145eddDf43f50276641b55bd3AD95944510021E);
+    IAaveOracle public constant ORACLE =
+        IAaveOracle(0xEBd36016B3eD09D4693Ed4251c67Bd858c3c7C9C);
+    address public constant AAVE_AVALANCHE_TREASURY =
+        0x5ba7fd868c40c16f7aDfAe6CF87121E13FC2F7a0;
+    address public constant INCENTIVES_CONTROLLER =
+        0x929EC64c34a17401F460460D4B9390518E5B473e;
+    IACLManager public constant ACL_MANAGER =
+        IACLManager(0xa72636CbcAa8F5FF95B2cc47F3CDEe83F3294a0B);
+
+    // **************************
+    // New eMode category (AVAX-like)
+    // **************************
+
+    uint8 public constant NEW_EMODE_ID = 2;
+    uint16 public constant NEW_EMODE_LTV = 9500; // 95% TODO review
+    uint16 public constant NEW_EMODE_LIQ_THRESHOLD = 9700; // 97% TODO review
+    uint16 public constant NEW_EMODE_LIQ_BONUS = 10250; // 2.5% TODO review
+    address public constant NEW_EMODE_ORACLE = address(0); // No custom oracle
+    string public constant NEW_EMODE_LABEL = 'AVAX-like'; // TODO review
+
+    // **************************
+    // New asset being listed (SAVAX)
+    // **************************
+
+    address public constant SAVAX = 0x2b2C81e08f1Af8835a78Bb2A90AE924ACE0eA4bE;
+    uint8 public constant SAVAX_DECIMALS = 18;
+    string public constant ASAVAX_NAME = 'Aave Avalanche sAVAX';
+    string public constant ASAVAX_SYMBOL = 'aAvasAVAX';
+    string public constant VDSAVAX_NAME = 'Aave Avalanche Variable Debt sAVAX';
+    string public constant VDSAVAX_SYMBOL = 'variableDebtAvasAVAX';
+    string public constant SDSAVAX_NAME = 'Aave Avalanche Stable Debt sAVAX';
+    string public constant SDSAVAX_SYMBOL = 'stableDebtAvasAVAX';
+    address public constant PRICE_FEED_SAVAX =
+        0x2854Ca10a54800e15A2a25cFa52567166434Ff0a;
+    address public constant ATOKEN_IMPL =
+        0xa5ba6E5EC19a1Bf23C857991c857dB62b2Aa187B;
+    address public constant VDTOKEN_IMPL =
+        0x81387c40EB75acB02757C1Ae55D5936E78c9dEd3;
+    address public constant SDTOKEN_IMPL =
+        0x52A1CeB68Ee6b7B5D13E0376A1E0E4423A8cE26e;
+    address public constant RATE_STRATEGY =
+        0x79a906e8c998d2fb5C5D66d23c4c5416Fe0168D6;
+    uint256 public constant LTV = 5000; // 50%
+    uint256 public constant LIQ_THRESHOLD = 6500; // 65%
+    uint256 public constant LIQ_BONUS = 11000; // 10%
+    uint256 public constant SUPPLY_CAP = 500_000; // ~$12.5 at price of 01/06/2022
+    uint256 public constant RESERVE_FACTOR = 1000; // 10%
+    uint256 public constant LIQ_PROTOCOL_FEE = 1000; // 10%
+
+    function listAssetAddingOracle() external onlyOwner {
+        // ----------------------------
+        // 1. New price feed on oracle
+        // ----------------------------
+
+        address[] memory assets = new address[](1);
+        assets[0] = SAVAX;
+        address[] memory sources = new address[](1);
+        sources[0] = PRICE_FEED_SAVAX;
+
+        ORACLE.setAssetSources(assets, sources);
+
+        // -----------------------------------------
+        // 2. Creation of new eMode on the Aave Pool
+        // -----------------------------------------
+
+        CONFIGURATOR.setEModeCategory(
+            NEW_EMODE_ID,
+            NEW_EMODE_LTV,
+            NEW_EMODE_LIQ_THRESHOLD,
+            NEW_EMODE_LIQ_BONUS,
+            address(0),
+            NEW_EMODE_LABEL
+        );
+
+        // ------------------------------------------------
+        // 3. Listing of sAVAX, with all its configurations
+        // ------------------------------------------------
+
+        ConfiguratorInputTypes.InitReserveInput[]
+            memory initReserveInputs = new ConfiguratorInputTypes.InitReserveInput[](
+                1
+            );
+        initReserveInputs[0] = ConfiguratorInputTypes.InitReserveInput({
+            aTokenImpl: ATOKEN_IMPL,
+            stableDebtTokenImpl: SDTOKEN_IMPL,
+            variableDebtTokenImpl: VDTOKEN_IMPL,
+            underlyingAssetDecimals: SAVAX_DECIMALS,
+            interestRateStrategyAddress: RATE_STRATEGY,
+            underlyingAsset: SAVAX,
+            treasury: AAVE_AVALANCHE_TREASURY,
+            incentivesController: INCENTIVES_CONTROLLER,
+            aTokenName: ASAVAX_NAME,
+            aTokenSymbol: ASAVAX_SYMBOL,
+            variableDebtTokenName: VDSAVAX_NAME,
+            variableDebtTokenSymbol: VDSAVAX_SYMBOL,
+            stableDebtTokenName: SDSAVAX_NAME,
+            stableDebtTokenSymbol: SDSAVAX_SYMBOL,
+            params: bytes('') // TODO
+        });
+
+        CONFIGURATOR.initReserves(initReserveInputs);
+
+        CONFIGURATOR.setSupplyCap(SAVAX, SUPPLY_CAP);
+
+        CONFIGURATOR.configureReserveAsCollateral(
+            SAVAX,
+            LTV,
+            LIQ_THRESHOLD,
+            LIQ_BONUS
+        );
+
+        CONFIGURATOR.setAssetEModeCategory(SAVAX, 2);
+
+        CONFIGURATOR.setReserveFactor(SAVAX, RESERVE_FACTOR);
+
+        CONFIGURATOR.setLiquidationProtocolFee(SAVAX, LIQ_PROTOCOL_FEE);
+
+        // ---------------------------------------------------------------
+        // 4. This contract renounces to both listing and risk admin roles
+        // ---------------------------------------------------------------
+
+        ACL_MANAGER.renounceRole(
+            ACL_MANAGER.ASSET_LISTING_ADMIN_ROLE(),
+            address(this)
+        );
+        ACL_MANAGER.renounceRole(ACL_MANAGER.RISK_ADMIN_ROLE(), address(this));
+    }
+}
---
+++ src/test/V3ListingByGuardian.sol
@@ -0,0 +1,181 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import 'forge-std/Test.sol';
+
+import {IPoolConfigurator, ConfiguratorInputTypes} from '../contracts/interfaces/IPoolConfigurator.sol';
+import {IACLManager} from '../contracts/interfaces/IACLManager.sol';
+import {AaveV3SAVAXListingSteward} from '../contracts/AaveV3SAVAXListingSteward.sol';
+import {AaveV3Helpers, ReserveConfig, ReserveTokens, IERC20} from './helpers/AaveV3Helpers.sol';
+
+contract V3ListingByGuardian is Test {
+    using stdStorage for StdStorage;
+
+    address public constant GUARDIAN_AVALANCHE =
+        0xa35b76E4935449E33C56aB24b23fcd3246f13470;
+
+    address public constant CURRENT_ACL_SUPERADMIN =
+        0x4365F8e70CF38C6cA67DE41448508F2da8825500;
+
+    address public constant SAVAX = 0x2b2C81e08f1Af8835a78Bb2A90AE924ACE0eA4bE;
+
+    address public constant SAVAX_WHALE =
+        0xf973C06c8964C1650e210c940db65Acbf7F2a48D;
+
+    address public constant DAIe = 0xd586E7F844cEa2F87f50152665BCbc2C279D8d70;
+
+    address public constant DAI_WHALE =
+        0xED2a7edd7413021d440b09D654f3b87712abAB66;
+
+    function setUp() public {}
+
+    function testAddSingleDistribution() public {
+        ReserveConfig[] memory allConfigsBefore = AaveV3Helpers
+            ._getReservesConfigs(false);
+
+        vm.startPrank(GUARDIAN_AVALANCHE);
+
+        AaveV3SAVAXListingSteward listingSteward = new AaveV3SAVAXListingSteward();
+
+        IACLManager aclManager = listingSteward.ACL_MANAGER();
+
+        aclManager.addAssetListingAdmin(address(listingSteward));
+        aclManager.addRiskAdmin(address(listingSteward));
+
+        listingSteward.listAssetAddingOracle();
+
+        vm.stopPrank();
+
+        ReserveConfig[] memory allConfigsAfter = AaveV3Helpers
+            ._getReservesConfigs(false);
+
+        ReserveConfig memory expectedAssetConfig = ReserveConfig({
+            symbol: 'sAVAX',
+            underlying: 0x2b2C81e08f1Af8835a78Bb2A90AE924ACE0eA4bE,
+            aToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            variableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            stableDebtToken: address(0), // Mock, as they don't get validated, because of the "dynamic" deployment on proposal execution
+            decimals: 18,
+            ltv: 5000,
+            liquidationThreshold: 6500,
+            liquidationBonus: 11000,
+            reserveFactor: 1000,
+            usageAsCollateralEnabled: true,
+            borrowingEnabled: false,
+            interestRateStrategy: address(0),
+            stableBorrowRateEnabled: false,
+            isActive: true,
+            isFrozen: false
+        });
+
+        AaveV3Helpers._validateReserveConfig(
+            expectedAssetConfig,
+            allConfigsAfter
+        );
+
+        AaveV3Helpers._noReservesConfigsChangesApartNewListings(
+            allConfigsBefore,
+            allConfigsAfter
+        );
+
+        AaveV3Helpers._validateReserveTokensImpls(
+            vm,
+            AaveV3Helpers._findReserveConfig(allConfigsAfter, 'sAVAX', false),
+            ReserveTokens({
+                aToken: listingSteward.ATOKEN_IMPL(),
+                stableDebtToken: listingSteward.SDTOKEN_IMPL(),
+                variableDebtToken: listingSteward.VDTOKEN_IMPL()
+            })
+        );
+
+        AaveV3Helpers._validateAssetSourceOnOracle(
+            SAVAX,
+            listingSteward.PRICE_FEED_SAVAX()
+        );
+
+        _validatePoolActionsPostListing(allConfigsAfter);
+    }
+
+    function _validatePoolActionsPostListing(
+        ReserveConfig[] memory allReservesConfigs
+    ) internal {
+        AaveV3Helpers._deposit(
+            vm,
+            SAVAX_WHALE,
+            SAVAX_WHALE,
+            SAVAX,
+            666 ether,
+            true,
+            AaveV3Helpers
+                ._findReserveConfig(allReservesConfigs, 'sAVAX', false)
+                .aToken
+        );
+
+        AaveV3Helpers._borrow(
+            vm,
+            SAVAX_WHALE,
+            SAVAX_WHALE,
+            DAIe,
+            222 ether,
+            2,
+            AaveV3Helpers
+                ._findReserveConfig(allReservesConfigs, 'DAI.e', false)
+                .variableDebtToken
+        );
+
+        // Only checking with 1 borrowing type, because we can understand that borrowing is disabled
+        // with the revert reason (BORROWING_NOT_ENABLED = 30)
+        try
+            AaveV3Helpers._borrow(
+                vm,
+                SAVAX_WHALE,
+                SAVAX_WHALE,
+                SAVAX,
+                5 ether,
+                2,
+                AaveV3Helpers
+                    ._findReserveConfig(allReservesConfigs, 'sAVAX', false)
+                    .stableDebtToken
+            )
+        {
+            revert('_testProposal() : BORROW_NOT_REVERTING');
+        } catch Error(string memory revertReason) {
+            require(
+                keccak256(bytes(revertReason)) == keccak256(bytes('30')),
+                '_testProposal() : INVALID_VARIABLE_REVERT_MSG'
+            );
+            vm.stopPrank();
+        }
+
+        vm.startPrank(DAI_WHALE);
+        IERC20(DAIe).transfer(SAVAX_WHALE, 300 ether);
+        vm.stopPrank();
+
+        // Not possible to borrow and repay when vdebt index doesn't changing, so moving 1s
+        skip(1);
+
+        AaveV3Helpers._repay(
+            vm,
+            SAVAX_WHALE,
+            SAVAX_WHALE,
+            DAIe,
+            IERC20(DAIe).balanceOf(SAVAX_WHALE),
+            2,
+            AaveV3Helpers
+                ._findReserveConfig(allReservesConfigs, 'DAI.e', false)
+                .variableDebtToken,
+            true
+        );
+
+        AaveV3Helpers._withdraw(
+            vm,
+            SAVAX_WHALE,
+            SAVAX_WHALE,
+            SAVAX,
+            type(uint256).max,
+            AaveV3Helpers
+                ._findReserveConfig(allReservesConfigs, 'sAVAX', false)
+                .aToken
+        );
+    }
+}
